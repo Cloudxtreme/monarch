35b87c7d765ecf1f4d4e0a6cb4a4f6fabedf7cbb
tim <tim@magnetic.io>
2015-02-10 13:50:36 +0100

    adds session cookies

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"os"
 	"strconv"
@@ -19,7 +20,8 @@ func main() {
 	port := flag.Int("port", 8080, "Sets the port to listen on")
 	depends_on_host := flag.String("depends_on_host", "", "Sets the host of another mesos-tester this mesos-tester depends on")
 	depends_on_port := flag.Int("depends_on_port", 0, "Sets the port of another mesos-tester this mesos-tester depends on")
-	version := flag.String("version", "0.2","Set a fake version number for testing purposes")
+	version := flag.String("version", "0.2", "Set a fake version number for testing purposes")
+	cookie_timout := flag.Int("cookie_timeout", 30, "Sets the cookie timout of the /session endpoint in seconds")
 
 	flag.Parse()
 
@@ -29,7 +31,7 @@ func main() {
 	envDepHost := os.Getenv("MONARCH_DEPENDS_ON_HOST")
 	envDepPort := os.Getenv("MONARCH_DEPENDS_ON_PORT")
 	envVersion := os.Getenv("MONARCH_VERSION")
-
+	envTimeout := os.Getenv("MONARCH_COOKIE_TIMEOUT")
 
 	if envPort != "" {
 		*port, _ = strconv.Atoi(envPort)
@@ -51,16 +53,47 @@ func main() {
 		*version = envVersion
 	}
 
+	if envTimeout != "" {
+		*cookie_timout, _ = strconv.Atoi(envTimeout)
+	}
+
 	fmt.Println("[magnetio-tester] --> Using dependency: " + dependency.Ip + ":" + strconv.Itoa(dependency.Port))
 
 	// start the REST API
-
 	r := gin.Default()
+
+	// create a session store
+	store := sessions.NewCookieStore([]byte("store"))
+
+	store.Options(sessions.Options{MaxAge: *cookie_timout})
+	r.Use(sessions.Sessions("MONARCH_SESSIONID", store))
+
 	// simple ping for health check
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong v2")
 	})
 
+	// uses a simple session cookie to check session stickiness
+	r.GET("/session", func(c *gin.Context) {
+		session := sessions.Default(c)
+
+		var count int
+		v := session.Get("count")
+
+		if v == nil {
+			count = 0
+		} else {
+			count = v.(int)
+			count += 1
+		}
+
+		session.Set("count", count)
+		session.Save()
+		c.JSON(200, gin.H{"count": count})
+
+	})
+
+	// answers after a random wait time
 	r.GET("/randomwait", func(c *gin.Context) {
 
 		myrand := random(100, 500)
